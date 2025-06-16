84e5a1029eb85f4bc03c9f5413971227ac252980
Rémi <debuyseremi@gmail.com>
2025-06-16 16:29:24 +0200

    jeu pendu V2

--- programming/javascript/Pendu/js.js
+++ programming/javascript/Pendu/js.js
@@ -1,4 +1,5 @@
-let mot="test";
+let mot=prompt("Saisir le mot à faire deviner svp")
+mot=mot.toLowerCase();
 let lettre =" ";
 let fail=0;
 let tabResultat=[];
@@ -6,22 +7,28 @@ let present=false;
 let x;
 let victoire=0;
 let tabEssai=[];
+let perdant=document.getElementById('perdu');
+let pendu=document.getElementById('mot');
+let result=document.getElementById('result');
+
 
 function preparatif(){
     fail=0;
     x=mot.length
      for(let j=0;j<x;j++){
         tabResultat[j]=" _ "
+        pendu.innerText+=tabResultat[j]
     }
 }
 
 preparatif()
 console.log(tabResultat)
 
+
 function saisie(){
  present=false;
  lettre = document.getElementById("lettre").value;
- while(lettre.length != 1 ){
+ while((lettre.length != 1) || (lettre.toUpperCase() == lettre.toLowerCase()) ){
     lettre=prompt("Erreur veuillez ressaisir la lettre svp")
  } 
  for(i=0;i<=x;i++){
@@ -29,19 +36,51 @@ function saisie(){
         tabResultat[i]=lettre;
         present=true;
         victoire++
-        if(victoire==x)
-        {
-            console.log("Bravo le mot était bien "+mot)
-        }
     }
  }
 if(present == false){
     tabEssai[fail]=lettre+" ";
+    perdant.innerText+=" "+tabEssai[fail];
     fail++;
 }
-console.log(tabResultat)
-console.log("Il y a eu "+fail+" échecs")
-console.log(tabEssai)
+console.log(tabResultat);
+console.log("Il y a eu "+fail+" échecs");
+console.log(tabEssai);
+pendu.textContent = "On recherche le mot suivant :"
+for(let j=0;j<x;j++){
+        pendu.innerText+=tabResultat[j]
+    }
+    switch (fail){
+        case 1:
+        document.getElementById('myImage').src='./Pendu/Pendu1.png';
+        break;
+        case 2:
+        document.getElementById('myImage').src='./Pendu/Pendu2.png';
+        break;
+        case 3:
+        document.getElementById('myImage').src='./Pendu/Pendu3.png';
+        break;
+        case 4:
+        document.getElementById('myImage').src='./Pendu/Pendu4.png';
+        break;
+        case 5:
+        document.getElementById('myImage').src='./Pendu/Pendu5.png';
+        break;
+        case 6:
+        document.getElementById('myImage').src='./Pendu/Pendu6.png';
+        break;
+        case 7:
+        document.getElementById('myImage').src='./Pendu/Pendu7.png';
+        break;
+        case 8:
+        document.getElementById('myImage').src='./Pendu/Pendu8.png';
+        break;
+        case 9:
+        document.getElementById('myImage').src='./Pendu/Pendu9.png';
+        result.innerText="Le jeu est terminé et le mot était : " + mot;
+        break;
+
+    }
 }
 
 
@@ -51,6 +90,7 @@ saisie()
 }
 else
 {
-    console.log("Le jeu est terminé")
+    result.innerText="Le jeu est terminé !";
+    console.log("Le jeu est terminé");
 }
 }
